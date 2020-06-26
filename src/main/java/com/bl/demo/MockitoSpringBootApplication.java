45e532bf5e74aaf0821369053c43cf973af7a20e
priyankagarad <garadpriyanka1995@gmail.com>
2020-06-26 15:00:47 +0530

    ADDED: Created Project

---
+++ src/main/java/com/bl/demo/MockitoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.bl.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoSpringBootApplication.class, args);
+    }
+
+}
